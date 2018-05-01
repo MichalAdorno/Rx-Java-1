7f8bf5144386c991fb29e20d90cccdec72aff7dc
lxuserb <zalewmic@gmail.com>
2018-05-01 17:01:47 +0200

    [Ex-1] Observable filtering, onNext, onClose, onError

--- src/com/myself/rx/Main.java
+++ src/com/myself/rx/Main.java
@@ -1,46 +1,13 @@
 package com.myself.rx;
 
 
-import io.reactivex.Observable;
-
+import com.myself.rx.examples.Example1;
 
 public class Main {
 
     public static void main(String[] args) throws InterruptedException {
-        Observable<String> source = Observable.create(x -> {
-            try {
-                x.onNext("Ada");
-                x.onNext("Basia");
-                x.onNext("Czesława");
-                x.onNext("Dusia");
-                x.onNext("Ela");
-                x.onNext("Feodosja");
-                x.onNext("Gudrun");
-                x.onNext("Henryka");
-                x.onNext("Iga");
-                x.onNext("Jadwiga");
-                x.onNext("Katarzyna");
-                x.onNext("Luba");
-                x.onNext("Łucja");
-                x.onNext("Mirosława");
-                x.onNext("Nadia");
-                x.onComplete();
-            }catch(Exception e){
-                x.onError(e);
-            }
-        });
 
-        Thread[] observers = new Thread[7];
-        for(int i = 0; i < observers.length; i++) {
-            final int j = i + 1;
-            Observable<String> filtered = source.filter(x -> x.length() <= j);
-            observers[i] = new Thread(new ObserverRunnable<String>(filtered, j));
-        }
-        for(int i = 0; i < observers.length; i++)
-            observers[i].start();
-        for(int i = 0; i < observers.length; i++)
-            observers[i].join();
-
+        Example1.show();
         System.out.println("Finished!");
     }
 }
---
+++ src/com/myself/rx/examples/Example1.java
@@ -0,0 +1,44 @@
+package com.myself.rx.examples;
+
+import com.myself.rx.ObserverRunnable;
+import io.reactivex.Observable;
+
+public class Example1 {
+    public static void show() throws InterruptedException {
+        Observable<String> source = Observable.create(x -> {
+            try {
+                x.onNext("Ada");
+                x.onNext("Basia");
+                x.onNext("Czesława");
+                x.onNext("Dusia");
+                x.onNext("Ela");
+                x.onNext("Feodosja");
+                x.onNext("Gudrun");
+                x.onNext("Henryka");
+                x.onNext("Iga");
+                x.onNext("Jadwiga");
+                x.onNext("Katarzyna");
+                x.onNext("Luba");
+                x.onNext("Łucja");
+                x.onNext("Mirosława");
+                x.onNext("Nadia");
+                x.onComplete();
+            }catch(Exception e){
+                x.onError(e);
+            }
+        });
+
+        Thread[] observers = new Thread[7];
+        for(int i = 0; i < observers.length; i++) {
+            final int j = i + 1;
+            Observable<String> filtered = source.filter(x -> x.length() <= j);
+            observers[i] = new Thread(new ObserverRunnable<String>(filtered, j));
+        }
+        for(int i = 0; i < observers.length; i++)
+            observers[i].start();
+        for(int i = 0; i < observers.length; i++)
+            observers[i].join();
+
+
+    }
+}
