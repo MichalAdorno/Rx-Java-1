277d4d16e6e0f2c28a7358823f2efcbba031ec49
lxuserb <zalewmic@gmail.com>
2018-05-04 21:16:49 +0200

    [Ex-27] Concurrency: subscribeOn() vs observeOn()

--- src/main/java/com/myself/rx/Main.java
+++ src/main/java/com/myself/rx/Main.java
@@ -33,8 +33,8 @@ public class Main {
 //        Example23.show();
 //        Example24.show();
 //        Example25.show();
-        Example26.show();
-
+//        Example26.show();
+        Example27.show();
 
 
         System.out.println("Finished!");
---
+++ src/main/java/com/myself/rx/examples/Example27.java
@@ -0,0 +1,64 @@
+package com.myself.rx.examples;
+
+
+import io.reactivex.Observable;
+import io.reactivex.schedulers.Schedulers;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.util.Arrays;
+import java.util.List;
+
+
+public class Example27 {
+
+    public static void show() throws Exception {
+
+        //----------------------------------------------------------------------------------
+        List<String> codes = Arrays.asList("WHISKEY/27653/TANGO", "6555/BRAVO", "232535/567675/FOXTROT");
+
+        Observable.fromIterable(codes)
+
+                .subscribeOn(Schedulers.io())
+                .flatMap(s -> Observable.fromArray(s.split("/")))
+                .doOnNext(s -> System.out.println("[1]> " + s + " on [" + Thread.currentThread().getName() + "]"))
+
+                .observeOn(Schedulers.computation())
+                .filter(s -> s.matches("[0-9]+"))
+                .map(Integer::valueOf)
+                .reduce(0, (sum, x) -> sum + x)
+                .doOnSuccess(i -> System.out.println("[1]> " + i + " on [" + Thread.currentThread().getName() + "]"))
+
+                .observeOn(Schedulers.io())
+                .map(i -> i.toString())
+                .doOnSuccess(s -> System.out.println("[1]> Writing: " + s + " on [" + Thread.currentThread().getName() + "]"))
+                .subscribe(s -> write(s, "/home/lxuserb/Desktop/output.txt"));
+
+
+        Thread.sleep(3000);
+
+
+        //----------------------------------------------------------------------------------
+
+    }
+
+    private static void write(String text, String path) {
+        BufferedWriter writer = null;
+        try {
+            //create a temporary file
+            File file = new File(path);
+            writer = new BufferedWriter(new FileWriter(file));
+            writer.append(text);
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                writer.close();
+            } catch (Exception e) {
+            }
+        }
+    }
+
+
+}
